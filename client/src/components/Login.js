ada698e7a8b8a4c971859589201ea36da8eacac1
jmodiale <remen.omodiale@outlook.com>
2022-03-01 14:01:54 +0000

    update

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,7 +8,7 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import MovingIcon from '@mui/icons-material/Moving';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -37,7 +37,7 @@ function Login() {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
+            <MovingIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
