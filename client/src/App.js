aad23db321ecb36c6cbad76638fbc3d17dd9463b
jmodiale <remen.omodiale@outlook.com>
2022-03-01 13:51:27 +0000

    frontend sigup and login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from './components/Register'
+import Login from "./components/Login";
+import { BrowserRouter,  Routes,  Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login/>} exact ></Route>
+					<Route path="/login" element={<Login/>}></Route>
+					<Route path="/register" element={<Register/>}></Route>
+					<Route path="*">Page not found</Route>
+				</Routes>
+			</BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/Register.js
@@ -0,0 +1,156 @@
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useNavigate} from 'react-router-dom';
+
+const theme = createTheme();
+
+function Register() {
+    const [firstName, setFirstname] = useState('')
+    const [lastName, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [error, setError] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+		// Validation
+
+		if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
+			return setError("Email address is invalid");
+		}
+
+		let regValue = /(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)(.{8,})/;
+		if (!regValue.test(password)) {
+			return setError ("Passwords must contain a number and at least 8 characters");
+		}
+
+		setError("");
+    }
+
+  const navigate = useNavigate();
+	const login = () => navigate.push("/login");
+
+  const errorMessage = error && <p style={{color: "red", textAlign: "center"}}>{error}</p>;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container 
+      component="main" 
+      maxWidth="xs" 
+      sx={{
+        boxShadow: 1,
+      }}
+      >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            paddingBottom: 3,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          {errorMessage}
+          <Box component="form"  onSubmit={handleSubmit}  sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  value ={firstName}
+                  onChange={(e)=>setFirstname(e.target.value)}
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  value={lastName}
+                  onChange={(e)=>setLastname(e.target.value)}
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  value ={email}
+                  onChange={(e)=>setEmail(e.target.value)}
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  value ={password}
+                  onChange={(e)=>setPassword(e.target.value)}
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link variant="body2" href="/login" onClick={login}>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default Register;
