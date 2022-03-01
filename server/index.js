6ba79b0889f240aa2c094883612060d43ed652f0
jmodiale <remen.omodiale@outlook.com>
2022-03-01 11:53:40 +0000

    server side setup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(5000, (req, res) => {
+    console.log('listening on port 5000')
+})
