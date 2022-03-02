2b795df970affeb8c2f4d92e90ca1469869f5e1d
samir <s-malik@live.co.uk>
2022-03-02 12:59:33 +0000

    Part 2 - autocomplete function working

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import { Container, makeStyles } from "@material-ui/core";
+import { Search } from "./Search";
+import dayjs from "dayjs";
+const useStyles = makeStyles({
+  // ...
+});
+const App = () => {
+  const classes = useStyles();
+  const [cityCode, setCityCode] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" className={classes.container}>
+      <Search setCityCode={setCityCode} />
+    </Container>
   );
-}
-
-export default App;
+};
+export { App };
