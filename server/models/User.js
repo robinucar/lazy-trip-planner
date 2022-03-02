11347692d9be2ffdd5601285e3da749f02674fe7
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-02 20:10:40 +0000

    new auth backend

---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User;
