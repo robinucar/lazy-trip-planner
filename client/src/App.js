2cf9385313728efbe6150d1fea2e1809f3df5fde
Cristian <cristian.christensen@gmail.com>
2022-03-02 15:32:49 +0100

    Part 2 - Check in/Check out working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Container, makeStyles } from "@material-ui/core";
 import { Search } from "./Search";
+import { DateFilters } from "./DateFilters";
 import dayjs from "dayjs";
 const useStyles = makeStyles({
   // ...
@@ -8,9 +9,17 @@ const useStyles = makeStyles({
 const App = () => {
   const classes = useStyles();
   const [cityCode, setCityCode] = useState(null);
+  const [checkInDate, setCheckInDate] = useState(dayjs());
+  const [checkOutDate, setCheckOutDate] = useState(dayjs().add(1, "day"));
   return (
     <Container maxWidth="sm" className={classes.container}>
       <Search setCityCode={setCityCode} />
+      <DateFilters
+        checkInDate={checkInDate}
+        checkOutDate={checkOutDate}
+        setCheckInDate={setCheckInDate}
+        setCheckOutDate={setCheckOutDate}
+      />
     </Container>
   );
 };
